f2c1b46d292c4823f738e97d8567126c10114c84
analyticbastard <albrecht.allenstein@gmail.com>
2014-02-11 12:50:05 +0100

    Add problem 8

---
+++ src/projecteuler/problem8.py
@@ -0,0 +1,10 @@
+'''
+Created on 10/02/2014
+
+@author: Javier
+'''
+
+print "It is easy to spot a candidate with large digits: 99879"
+print "whose product is 40824. It turns out this works."
+print "A nice implementation would be to spot 9's and look around their position"
+print "storing the maximum of their products" 
