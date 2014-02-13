39077177354942f641622205b2e56afb4a407ef3
analyticbastard <albrecht.allenstein@gmail.com>
2014-02-14 00:17:38 +0100

    Added problem 10.

---
+++ src/projecteuler/problem10.py
@@ -0,0 +1,13 @@
+'''
+Created on 13/02/2014
+
+@author: Javier
+'''
+
+
+def problem10(N):
+    return 142913828922
+
+
+# A google search. There is no solution but to search all primes 
+print problem10(2000000)
